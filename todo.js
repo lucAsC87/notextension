e35967d3cb113231cef47a858474c0f838121540
lucAsC87 <lucas2187@gmail.com>
2023-03-20 19:15:45 +0100

    Small modifications to js file

--- todo.js
+++ todo.js
@@ -1,20 +1,17 @@
-let cookieString = document.getElementById("ft-list");
-document.cookie = `list = what; expires=Fri, 17 Mar 2023 12:00:00 UTC`;
-document.getElementById("new_do").addEventListener("click", addTask)
+document.getElementById("new_do").addEventListener("click", addTask);
 
 function addTask() {
-    let task = prompt("Add new task:");
-    if (task) {
-        const newDiv = document.createElement("div");
-        const newTask = document.createTextNode(task);
-        newDiv.appendChild(newTask);
-        const taskList = document.getElementById("ft_list");
-        taskList.insertBefore(newDiv, taskList.children[0]);
-        newDiv.addEventListener("click", function() {
-            if (confirm("Remove task?")) {
-                newDiv.remove()           
-                }
-            });
-        }
+  let task = prompt("Add new task:");
+  if (task) {
+    const newListEl = document.createElement("li");
+    const newTask = document.createTextNode(task);
+    newListEl.appendChild(newTask);
+    const taskList = document.getElementById("ft_list");
+    taskList.insertBefore(newListEl, taskList.children[0]);
+    newListEl.addEventListener("click", function () {
+      if (confirm(`Remove ${newListEl.textContent}?`)) {
+        newListEl.remove();
+      }
+    });
+  }
 }
-
